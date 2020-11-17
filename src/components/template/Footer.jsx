7600be90c18c780f039b6023c9b04e7d8ef16d20
Fernando <fernandosilva@unochapeco.edu.br>
2020-11-17 03:28:14 -0300

    up

--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -5,6 +5,6 @@ import React from 'react'
 export default props =>
     <footer className={"footer"}>
         <span>
-            <strong> <span className={"setColor"}>FacilitaMed</span> </strong><i className={"fa fa-heart"} style={{color: '#89C5C6'}}></i>
+            <strong> <span className={"setColor"}>FacilitaMed</span> </strong> <i className={"fa fa-heart"} style={{color: '#89C5C6'}}></i> <span className={"setColor"}><i>@Prototype</i></span>
         </span>
     </footer>
